10cb9c371492d76fe56eb906f2cbf696de273ebb
laysalimaa <laysalimadepinho@gmail.com>
2026-04-18 18:30:01 -0300

    feat: implementação do database helper com criação da tabela visitas

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,36 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  DatabaseHelper._();
+
+  static final DatabaseHelper instance = DatabaseHelper._();
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, 'carangode_visits.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE visitas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        sincronizado INTEGER NOT NULL DEFAULT 0
+      )
+    ''');
+  }
+}
