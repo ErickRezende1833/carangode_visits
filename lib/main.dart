b6ba928f7f5feaf085060c894e263789c74b142c
Erick <persona0rcc@gmail.com>
2026-05-18 11:35:20 -0300

    Fix: Mais correcoes de regras de negocio e organização do codigo

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       
-      initialRoute: '/campo',
+      initialRoute: '/login',
         routes: {
           '/login': (context) => LoginView(),
           '/campo': (context) => const HomeCampoView(),
